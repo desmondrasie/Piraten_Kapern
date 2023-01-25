a901047ffae4d57b5b03184f83859d743ad702df
desmos <desmond.rasie@live.ca>
2023-01-25 18:28:24 -0500

    Implemented the feature rollEight()

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,13 +1,15 @@
 import pk.Dice;
+import pk.Faces;
+import pk.Score;
+import java.util.Arrays;
 
 public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
-        System.out.println("I'm rolling a dice");
+        System.out.println("Player 1 Rolls 8 Dice: ");
         Dice myDice = new Dice();
-        System.out.println(myDice.roll());
-        System.out.println("That's all folks!");
+        System.out.println(Arrays.toString(myDice.rollEight()));
     }
-    
+
 }
---
+++ src/main/java/pk/Score.java
@@ -0,0 +1,14 @@
+package pk;
+import java.util.Arrays;
+
+public class Score {
+
+    public int roundScore() {
+        for (Faces i: Dice.rollEight()){
+            System.out.println("test");
+        }
+        return 1;
+
+    }
+
+}
