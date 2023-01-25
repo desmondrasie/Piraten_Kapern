e10b3d43c8dfcafc4a0e5c696614f70dd4abd7a4
desmos <desmond.rasie@live.ca>
2023-01-25 18:28:24 -0500

    Implemented the feature "rollEight\(\)

--- src/main/java/pk/Dice.java
+++ src/main/java/pk/Dice.java
@@ -4,12 +4,19 @@ import java.util.Random;
 
 public class Dice {
 
-    public Faces roll() {
+    public static Faces roll() {
         int howManyFaces = Faces.values().length;
-        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
-        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
+//        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
+//        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         Random bag = new Random();
         return Faces.values()[bag.nextInt(howManyFaces)];
     }
-    
+
+    public static Faces[] rollEight() {
+        Faces[] rollOutcomes = new Faces[8];
+        for (int i = 0; i < 8; i++){
+            rollOutcomes[i] = roll();
+        }
+        return rollOutcomes;
+    }
 }
